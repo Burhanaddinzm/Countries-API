cf29b5ff47c044d06232e1dfe48e343eeabd19a2
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-01-09 21:29:37 +0400

    transition to scss

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const searchBtn = document.getElementById("search-btn");
 const dropdown = document.querySelector(".dropdown");
 const chevron = document.querySelector(".chevron");
 
+// Dropdown Children
+const dropdownFilter = document.querySelector(".filter");
+const chevronImg = document.querySelector(".chevron img");
+
 // Dropdown Menu
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
@@ -16,18 +20,14 @@ const cardContainer = document.querySelector(".card-container");
 
 let fetchedData = [];
 
-const fetchData = () => {
-  fetch("https://restcountries.com/v3.1/all")
-    .then((res) => {
-      cardContainer.innerHTML = "";
-      loadMsg.classList.remove("hidden");
-      return res.json();
-    })
-    .then((data) => {
-      loadMsg.classList.add("hidden");
-      displayData(data);
-      fetchedData = data;
-    });
+const fetchData = async () => {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  cardContainer.innerHTML = "";
+  loadMsg.classList.remove("hidden");
+  const data = await response.json();
+  loadMsg.classList.add("hidden");
+  displayData(data);
+  fetchedData = data;
 };
 
 const displayData = (data) => {
@@ -41,6 +41,7 @@ const displayData = (data) => {
     const flag = element.flags.png;
 
     cardContainer.innerHTML += `   <div class="card">
+    <a href="./countryPage/country.html">
       <div class="img-container">
       <img src="${flag}" alt="flag" />
       </div>
@@ -54,34 +55,34 @@ const displayData = (data) => {
       }</span></p>
         </div>
         </div>
+        </a>
         </div>`;
   });
 };
 
 const filterName = (name) => {
   const filteredData = fetchedData.filter((item) => {
-    return item.name.common.toLowerCase() === name.toLowerCase();
+    return item.name.common.toLowerCase().startsWith(name.toLowerCase());
   });
   name ? displayData(filteredData) : displayData(fetchedData);
 };
 
-const filterRegion = () => {
-  dropdown.addEventListener("click", (e) => {
-    dropdownMenu.classList.toggle("hidden");
-    chevron.classList.toggle("inverted");
+// Filter Region
+dropdown.addEventListener("click", (e) => {
+  dropdownMenu.classList.toggle("hidden");
+  chevron.classList.toggle("inverted");
 
-    const clicked = e.target;
-    const selectedRegion = clicked.dataset.region;
+  const clicked = e.target;
+  const selectedRegion = clicked.dataset.region;
 
-    if (selectedRegion) {
-      const filteredData = fetchedData.filter((item) => {
-        return item.region === selectedRegion;
-      });
+  if (selectedRegion) {
+    const filteredData = fetchedData.filter((item) => {
+      return item.region === selectedRegion;
+    });
 
-      displayData(filteredData);
-    }
-  });
-};
+    displayData(filteredData);
+  }
+});
 
 searchBtn.addEventListener("click", () => {
   const inputValue = input.value.trim();
@@ -90,5 +91,23 @@ searchBtn.addEventListener("click", () => {
   filterName(inputValue);
 });
 
-filterRegion();
+"click keypress".split(" ").forEach((event) => {
+  window.addEventListener(event, (e) => {
+    if (e.code === "Enter") {
+      const inputValue = input.value.trim();
+      input.value = "";
+
+      filterName(inputValue);
+    }
+    if (
+      e.target !== dropdown &&
+      e.target !== dropdownFilter &&
+      e.target !== chevronImg
+    ) {
+      dropdownMenu.classList.add("hidden");
+      chevron.classList.remove("inverted");
+    }
+  });
+});
+
 fetchData();
